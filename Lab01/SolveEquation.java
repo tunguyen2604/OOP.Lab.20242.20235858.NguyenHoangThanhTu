62c82f13b592ef1a93dd4567c6b5bed49200e82e
tunguyen2604 <tu.nht235858@sis.hust.edu.vn>
2025-03-01 16:57:16 +0700

    Lab01

---
+++ Lab01/SolveEquation.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+public class SolveEquation {
+    Object [] options = {"The first-degree equation (linear equation) with one variable",
+    "The system of first-degree equations (linear system) with two variables",
+    "The second-degree equation with one variable"};
+    int n = JOptionPane.showOptionDialog(frame, "What kind of equation would you like to solve?",
+                            "Choose the kind of equation", )
+}
