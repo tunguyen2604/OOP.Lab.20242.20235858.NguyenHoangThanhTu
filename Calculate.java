241d7546d3a2876c842bb41ca102179eba354e42
tunguyen2604 <tu.nht235858@sis.hust.edu.vn>
2025-03-01 16:36:42 +0700

    Complete assignement 2.2.5

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,5 @@
 import javax.swing.JOptionPane;
-public class ShowTwoNumbers {
+public class Calculate {
     public static void main(String[] args) {
         String strNum1, strNum2;
         String strNotification = "The sum, difference, product, and quotient of ";
