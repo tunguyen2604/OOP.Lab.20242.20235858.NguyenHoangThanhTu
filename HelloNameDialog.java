c44c55d0ca3bcf2e5c9da2a8e0c39ef2bb283073
tunguyen2604 <tu.nht235858@sis.hust.edu.vn>
2025-03-01 16:05:41 +0700

    Complete assignment 2.2.3

---
+++ HelloNameDialog.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+public class HelloNameDialog{
+    public static void main(String[] args) {
+        String result;
+        result = JOptionPane.showInputDialog("Please enter yourname: ");
+        JOptionPane.showMessageDialog(null, "Hi " + result +"!");
+        System.exit(0);
+    }
+}
